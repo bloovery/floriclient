54f01ea616795c143c56558c81aca3180d9a7ec8
Daniele Lenares <daniele.lenares@gmail.com>
2020-11-11 11:49:20 +0100

    feat: add api url and version

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 class FloriClient {
   clientId: string;
   clientSecret: string;
+  apiUrl: string;
+  apiVersion: string;
 
   constructor(clientId: string, clientSecret: string) {
     this.clientId = clientId;
     this.clientSecret = clientSecret;
+
+    this.apiUrl = 'https://api.floricode.com';
+    this.apiVersion = 'v2';
   }
 }
