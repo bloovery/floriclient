00e662d36c0c50155ac034b7544ebe78638d31e3
Daniele Lenares <daniele.lenares@gmail.com>
2020-11-11 13:06:25 +0100

    feat: create get products method

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
-class FloriClient {
+import ApiClient from './api.class'
+
+export = class FloriClient {
   clientId: string;
   clientSecret: string;
   apiUrl: string;
   apiVersion: string;
+  apiClient: ApiClient;
 
   constructor(clientId: string, clientSecret: string) {
     this.clientId = clientId;
     this.clientSecret = clientSecret;
 
-    this.apiUrl = 'https://api.floricode.com';
-    this.apiVersion = 'v2';
+    this.apiClient = new ApiClient(this.clientId, this.clientSecret);
+  }
+
+  /**
+   * Retrieve all products
+   */
+  getProducts () {
+    return this.apiClient.call('/VBN/Product')
   }
 }
