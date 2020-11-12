49702ff8d328c988d228e37aaa7fb206143a0109
Daniele Lenares <daniele.lenares@gmail.com>
2020-11-12 08:23:59 +0100

    feat(productGroup): add get translation

--- src/models/productGroup.class.ts
+++ src/models/productGroup.class.ts
@@ -1,9 +1,13 @@
+import ApiClient from '../api.class'
+
 export default class ProductGroup {
   id: number;
   description: string;
   entryDate: Date;
   changeDateTime: Date;
   expiryDate: Date;
+
+  private apiClient: ApiClient
 
   constructor (
     id: number,
@@ -17,5 +21,17 @@ export default class ProductGroup {
     this.entryDate = entryDate;
     this.changeDateTime = changeDateTime;
     this.expiryDate = expiryDate;
+
+    this.apiClient = ApiClient.getInstance()
+  }
+
+  async getTranslation (languageCode: string) {
+    const { value: nameResponse } = await this.apiClient.call('/VBN/Name', {
+      filter: `involved_code_list_id eq 16 and language_id eq '${languageCode}' and code_list_item_id eq '${this.id}'`
+    })
+    if (nameResponse.length === 1) {
+      return nameResponse[0].name_or_translation
+    }
+    return null
   }
 }
